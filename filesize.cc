37134a138da38a07c7ebbb7d7e1217ffac04b2a4
Camden <ch948118@ohio.edu>
2022-01-13 20:09:40 -0500

    Final

--- filesize.cc
+++ filesize.cc
@@ -3,37 +3,42 @@
 using namespace std;
 
 // output format to use IDENTICALLY:
-//BSH:Saru> ./filesize input.3lines input.1line 
+//BSH:Saru> ./filesize input.3lines input.1line
 //program: ./filesize
 // input.3lines: 3
 // input.1line: 1
 
-int main( int argc, char* argv[] )
+int main(int argc, char *argv[])
 {
     // just to get you started, this is how to refer to the arguments that were passed
     //for (int arg = 0; arg < argc; ++arg)
-            //std::cout << "argv[" << arg << "]: " << argv[arg] << '\n' ;
+    //std::cout << "argv[" << arg << "]: " << argv[arg] << '\n' ;
 
+    cout << "program: ./filesize" << endl;
+    for (int i = 1; i < argc; i++)
+    {
+        ifstream ifs;
 
+        ifs.open(argv[i]);
 
-    for(int i = 1; i < argc; i++){
-    ifstream ifs;
+        if (!ifs)
+        {
+            cout << " " << argv[i] << ": "
+                 << "-1" << endl;
+        }
+        else{
 
-    ifs.open(argv[i]);
+        string line;
+        int numlines = 0;
 
-
-    string line;
-    int numlines = 0;
-
-        while(getline(ifs,line)){
+        while (getline(ifs, line))
+        {
             numlines++;
         }
 
-    cout << numlines<<endl;
-    //return numlines;
+        cout << " " << argv[i] << ": " << numlines << endl;
+        }
     }
-
-
 
     exit(0); // this means that the program executed correctly!
 }
